74c0da14cb211c1ce53348d83a256f573f7a2036
Darren Cobian <darrencobian98@gmail.com>
2022-01-17 02:08:55 -0800

    Completed parsing

--- relayer_scraper.py
+++ relayer_scraper.py
@@ -2,33 +2,39 @@ import requests
 import pandas as pd
 import numpy as np
 import json
+import time
 
 def main():
     transferMS()
     # receiveMS()
 
 def transferMS():
+    start_time = time.time()
     offset = 0
-    while offset <= 0:
+
+    while offset <= 37800:
         urlTransfer = "https://api.mintscan.io/v1/relayer/cosmoshub-4/channel-141/txs?limit=45&offset={}&messageType=TRANSFER".format(str(offset))
         headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
         response = requests.get(urlTransfer, headers = headers)
         data_json = response.json()
-        
-        if response.status_code == 200 and data_json["txs"][0]["data"]["code"] == 0:
-            time = data_json["txs"][0]["header"]["timestamp"]
-            txhash = data_json["txs"][0]["data"]["txhash"]
-            amount = data_json["txs"][0]["data"]["tx"]["body"]["messages"][0]["token"]["amount"]
-            senderAddress = data_json["txs"][0]["data"]["tx"]["body"]["messages"][0]["sender"]
-            receiverAddress = data_json["txs"][0]["data"]["tx"]["body"]["messages"][0]["receiver"]
-            print("Time", time)
-            print("TxHash", txhash)
-            print("Amount", float(amount)*.000001)
-            print("Sender Address", senderAddress)
-            print("Receiver Address", receiverAddress)
 
+        for i in range(45):
+            if response.status_code == 200 and data_json["txs"][0]["data"]["code"] == 0:
+                print("Counter:", i)
 
+                timestamp = data_json["txs"][i]["header"]["timestamp"]
+                txhash = data_json["txs"][i]["data"]["txhash"]
+                amount = data_json["txs"][i]["data"]["tx"]["body"]["messages"][0]["token"]["amount"]
+                senderAddress = data_json["txs"][i]["data"]["tx"]["body"]["messages"][0]["sender"]
+                receiverAddress = data_json["txs"][i]["data"]["tx"]["body"]["messages"][0]["receiver"]
+                
+                print("Time", timestamp)
+                print("TxHash", txhash)
+                print("Amount", float(amount)*.000001)
+                print("Sender Address", senderAddress)
+                print("Receiver Address", receiverAddress)
         offset = offset + 45
+    print("--- %s seconds ---" % (time.time() - start_time))
 
 def receiveMS():
     ...
