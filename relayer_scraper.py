c57a549a3208ef9a96cc279a1b94c711f749aa8c
Darren Cobian <darrencobian98@gmail.com>
2022-01-18 02:39:30 -0800

    received not working

--- relayer_scraper.py
+++ relayer_scraper.py
@@ -1,10 +1,11 @@
 import requests
 import time
 import csv
+import json
 
 def main():
-    transferMS()
-    # receiveMS()
+    # transferMS()
+    receiveMS()
 
 def transferMS():
 
@@ -42,7 +43,49 @@ def transferMS():
         csvTransfer.close()
 
 def receiveMS():
-    ...
+    start_time = time.time()
+    offset = 0
+    txSet = set()
+    csvReceived = open('received_transactions.csv', 'w+')
+
+    try:
+        writer = csv.writer(csvReceived, lineterminator='\n')
+        writer.writerow(('txHash', 'SenderAddress', 'ReceiverAddress', 'Amount', 'Time'))
+        while offset <= 45:
+            urlReceive = "https://api.mintscan.io/v1/relayer/cosmoshub-4/channel-141/txs?limit=45&offset={}&messageType=RECEIVE".format(str(offset))
+            headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+            response = requests.get(urlReceive, headers = headers)
+            data_json = response.json()
+
+            for i in range(45):
+                if response.status_code == 200 and data_json["txs"][0]["data"]["code"] == 0:
+                    txhash = data_json["txs"][i]["data"]["txhash"]
+                    if txhash not in txSet:
+                        txSet.add(txhash)
+                        timestamp = data_json["txs"][i]["header"]["timestamp"]
+                        jsonString = data_json["txs"][i]["data"]["raw_log"]
+                        aDict = json.loads(jsonString)
+                        bDict = json.loads(aDict[1]["events"][1]["attributes"][0]["value"])
+                        amount = bDict["amount"]
+                        senderAddress = bDict["sender"]
+                        receiverAddress = bDict["receiver"]
+                        print(amount)
+                        print(senderAddress)
+                        print(receiverAddress)
+                        # jsonString = data_json["txs"][i]["data"]["logs"][1]["events"][1]["attributes"][0]["value"]
+                        # aDict = json.loads(jsonString)
+                        # amount = aDict["amount"]
+                        # senderAddress = aDict["sender"]
+                        # receiverAddress = aDict["receiver"]
+
+                        writer.writerow((txhash, senderAddress, receiverAddress, float(amount)*.000001, timestamp))
+            offset = offset + 45
+        print("--- %s seconds ---" % (time.time() - start_time))
+    except Exception as e:
+        print("Error: " + str(e))
+    finally:
+        print('Done.')
+        csvReceived.close()
 
 if __name__ == '__main__':
     main()
