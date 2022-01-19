27dfda5892799d276bff5af3e8cf15bf617c2850
Darren Cobian <darrencobian98@gmail.com>
2022-01-19 00:33:45 -0800

    Received transactions complete

--- relayer_scraper.py
+++ relayer_scraper.py
@@ -24,7 +24,7 @@ def transferMS():
             data_json = response.json()
 
             for i in range(45):
-                if response.status_code == 200 and data_json["txs"][0]["data"]["code"] == 0:
+                if response.status_code == 200 and data_json["txs"][i]["data"]["code"] == 0:
                     txhash = data_json["txs"][i]["data"]["txhash"]
                     if txhash not in txSet:
                         txSet.add(txhash)
@@ -51,32 +51,25 @@ def receiveMS():
     try:
         writer = csv.writer(csvReceived, lineterminator='\n')
         writer.writerow(('txHash', 'SenderAddress', 'ReceiverAddress', 'Amount', 'Time'))
-        while offset <= 45:
+        while offset <= 42885:
             urlReceive = "https://api.mintscan.io/v1/relayer/cosmoshub-4/channel-141/txs?limit=45&offset={}&messageType=RECEIVE".format(str(offset))
             headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
             response = requests.get(urlReceive, headers = headers)
             data_json = response.json()
 
             for i in range(45):
-                if response.status_code == 200 and data_json["txs"][0]["data"]["code"] == 0:
+                if response.status_code == 200 and data_json["txs"][i]["data"]["code"] == 0:
                     txhash = data_json["txs"][i]["data"]["txhash"]
                     if txhash not in txSet:
                         txSet.add(txhash)
                         timestamp = data_json["txs"][i]["header"]["timestamp"]
-                        jsonString = data_json["txs"][i]["data"]["raw_log"]
-                        aDict = json.loads(jsonString)
-                        bDict = json.loads(aDict[1]["events"][1]["attributes"][0]["value"])
-                        amount = bDict["amount"]
-                        senderAddress = bDict["sender"]
-                        receiverAddress = bDict["receiver"]
-                        print(amount)
-                        print(senderAddress)
-                        print(receiverAddress)
-                        # jsonString = data_json["txs"][i]["data"]["logs"][1]["events"][1]["attributes"][0]["value"]
-                        # aDict = json.loads(jsonString)
-                        # amount = aDict["amount"]
-                        # senderAddress = aDict["sender"]
-                        # receiverAddress = aDict["receiver"]
+                        for j in range(1, len(data_json["txs"][i]["data"]["logs"])):
+                                if data_json["txs"][i]["data"]["logs"][j]["events"][0]["attributes"][0]["value"] == "/ibc.core.channel.v1.MsgRecvPacket":
+                                    jsonString = data_json["txs"][i]["data"]["logs"][j]["events"][1]["attributes"][0]["value"]
+                                    aDict = json.loads(jsonString)
+                                    amount = aDict["amount"]
+                                    senderAddress = aDict["sender"]
+                                    receiverAddress = aDict["receiver"]
 
                         writer.writerow((txhash, senderAddress, receiverAddress, float(amount)*.000001, timestamp))
             offset = offset + 45
