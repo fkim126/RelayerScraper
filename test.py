30ec980ae963cc4a60fc3abae88b6151e434c9be
fkim126 <frankkim126@gmail.com>
2022-01-25 21:15:14 -0800

    cleaned files

--- test.py
+++
@@ -1,5 +0,0 @@
-import urllib3
-
-http = urllib3.PoolManager()
-r = http.request('GET', 'https://api.mintscan.io/v1/relayer/cosmoshub-4/channel-141/txs?limit=45&offset=0&messageType=TRANSFER')
-print(r.status)
